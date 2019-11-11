243c80e65ac1bfc99aad3c236ba56ce3fda933cb
Nicolas Vega <nrv0926@gmail.com>
2019-11-11 00:40:55 -0500

    update

---
+++ my-app/src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <div className='colors position-relative footer footer-100 footer-mgn navbar p-0'>
+      <span className='text-muted' />
+    </div>
+  );
+}
+
+export default Footer;
